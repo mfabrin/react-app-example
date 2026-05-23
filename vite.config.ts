10476679a70f4df9847a577755c2d9440b1ec099
Massimiliano Fabrin <fabrinmassimiliano@proton.me>
2026-05-23 15:36:05 +0200

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,10 @@
-import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-
-const root = process.cwd();
 
 // https://vite.dev/config/
 export default defineConfig({
 	resolve: {
-		alias: [
-			{ find: "@components", replacement: path.resolve(root, "src/components") },
-			{ find: "@services", replacement: path.resolve(root, "src/services") },
-			{ find: "@views", replacement: path.resolve(root, "src/views") },
-		],
+		tsconfigPaths: true,
 	},
 	plugins: [react()],
 });
